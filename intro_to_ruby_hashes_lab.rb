809fb2b8050ef04834426e4e536fa2046cf48eeb
Mitchell T Alton <mitchell.alton@live.com>
2020-01-03 05:37:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,9 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
- if hash["hello"]
-   hash["hello"] += 1 
+ if hash[key]
+   hash[key] += 1 
  else
-   hash["hello"] = 1 
+   hash[key] = 1 
  end
 end
